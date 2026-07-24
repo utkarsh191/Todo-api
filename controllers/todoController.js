288e9c0b49b0494a2b802d65bc58952a732bb9cc
UTKARSH KESHARWANI <utkarsh98399426@gmail.com>
2026-07-25 00:14:20 +0530

    Add get single todo api

--- controllers/todoController.js
+++ controllers/todoController.js
@@ -33,3 +33,29 @@ exports.getAllTodos = async(req, res) => {
     });
   }
 };
+
+exports.getSingleTodo = async (req, res) => {
+  try {
+    const todo = await Todo.findOne({
+      _id: req.params.id,
+      user: req.user.id,
+    });
+
+    if (!todo) {
+      return res.status(404).json({
+        success: false,
+        message:"Todo not found",
+      });
+    }
+
+    return res.status(200).json({
+      success: true,
+      todo,
+    });
+  } catch (error) {
+    return res.status(500).json({
+    success: false,
+    message: "Server Error",
+    });
+  }
+};
