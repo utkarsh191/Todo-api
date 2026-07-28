56fe87b687fd2ff27d7d317790523f4e1f246f5b
UTKARSH KESHARWANI <utkarsh98399426@gmail.com>
2026-07-28 12:44:18 +0530

    Add separate profile and cover image upload functionality

--- controllers/uploadController.js
+++ controllers/uploadController.js
@@ -5,3 +5,11 @@ exports.uploadImages = (req, res) => {
     files: req.files,
   });
 };
+
+exports.uploadProfileAndCover = (req, res) => {
+  res.status(200).json({
+    success: true,
+    message: "Profile and Cover uploaded successfully",
+    files: req.files,
+  });
+};
--- routes/uploadRoutes.js
+++ routes/uploadRoutes.js
@@ -3,8 +3,15 @@ const router = express.Router();
 
 const upload = require("../middleware/upload");
  
-const { uploadImages } = require("../controllers/uploadController");
+const { uploadImages , uploadProfileAndCover,} = require("../controllers/uploadController");
 
 router.post("/", upload.array("image", 5), uploadImages);
+
+router.post("/profile-cover", upload.fields([
+    { name: "profile", maxCount: 1 },
+    { name: "cover", maxCount: 1 },
+  ]),
+  uploadProfileAndCover
+);
 
 module.exports = router;
