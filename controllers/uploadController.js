47803d21788c64efd3f86da2cb1d566d10823269
UTKARSH KESHARWANI <utkarsh98399426@gmail.com>
2026-07-28 23:32:43 +0530

    Implement Multer and Cloudinary integration with profile, cover and multiple file uploads

--- controllers/uploadController.js
+++ controllers/uploadController.js
@@ -1,37 +1,78 @@
 const cloudinary = require("../config/cloudinary");
 const fs = require("fs");
+const User = require("../models/User");
+const asyncHandler = require("../utils/asyncHandler");
 
-exports.uploadImages = async (req, res) => {
+exports.uploadImages = asyncHandler(async (req, res) => {
   const result = await cloudinary.uploader.upload(req.files[0].path);
+
   fs.unlinkSync(req.files[0].path);
+
   res.status(200).json({
     success: true,
     message: "Files uploaded successfully",
     imageUrl: result.secure_url,
     publicId: result.public_id,
   });
-};
+});
 
-exports.uploadProfileAndCover = (req, res) => {
+exports.uploadProfileAndCover = asyncHandler(async(req, res) => {
+
+  const profileResult = await cloudinary.uploader.upload(
+    req.files.profile[0].path
+  );
+
+  const coverResult = await cloudinary.uploader.upload(
+    req.files.cover[0].path
+  );
+
+  fs.unlinkSync(req.files.profile[0].path);
+  fs.unlinkSync(req.files.cover[0].path);
+
   res.status(200).json({
     success: true,
     message: "Profile and Cover uploaded successfully",
-    files: req.files,
-  });
-};
+    
+    profileImage: profileResult.secure_url,
+    profilePublicId: profileResult.public_id,
 
-exports.registerUser = (req, res) => {
+    coverImage: coverResult.secure_url,
+    coverPublicId: coverResult.public_id,
+
+  });
+});
+
+exports.registerUser = asyncHandler(async (req, res) => {
+
+  const user = await User.create(req.body);
+
   res.status(200).json({
     success: true,
     message: "User registered successfully",
-    data: req.body,
+    user,
   });
-};
+});
 
-exports.uploadAnyFiles = (req, res) => {
+exports.uploadAnyFiles = asyncHandler(async (req, res) => {
+
+  const uploadedFiles = [];
+
+  for (const file of req.files) {
+
+    const result = await cloudinary.uploader.upload(file.path);
+
+    uploadedFiles.push({
+      imageUrl: result.secure_url,
+      publicId: result.public_id,
+    });
+
+    fs.unlinkSync(file.path);
+  }
+
   res.status(200).json({
-    success:true,
+    success: true,
     message: "Files uploaded successfully",
-    files: req.files,
+    files: uploadedFiles,
   });
-}
+
+});
